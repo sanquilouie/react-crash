0da22806ff241e03cac17655bc3dee0c803c5a96
louieeeee <louiesanqui0518@gmail.com>
2024-08-04 18:04:32 +0800

    Installed ReactRouter

--- react-course/src/App.jsx
+++ react-course/src/App.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards"
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+import HomePage from './pages/HomePage';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<HomePage />} />)
+)
 
 const App = () => {
-  return (
-    <>
-    <Navbar  />
-    <Hero />
-    <HomeCards />
-    <JobListings />
-    <ViewAllJobs />
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
--- react-course/tailwind.config.js
+++ react-course/tailwind.config.js
@@ -13,6 +13,6 @@ export default {
         '70/30': '70% 28%'
       }
     },
-  },
+  }, 
   plugins: [],
 }
