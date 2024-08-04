2b349df6a615d7dd50cb345e645057e7074b1069
louieeeee <louiesanqui0518@gmail.com>
2024-08-05 06:26:10 +0800

    Added MainLayout, HomePage

--- react-course/src/App.jsx
+++ react-course/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import HomePage from './pages/HomePage';
+import MainLayout from './layout/MainLayout';
+import JobsPage from './pages/JobsPage';
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import NotFoundPage from './pages/NotFoundPage';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={<HomePage />} />)
-)
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout />}>
+    <Route index element={<HomePage />} />
+    <Route path='/jobs' element={<JobsPage />} />
+    <Route path='*' element={<NotFoundPage />} />
 
+  </Route>
+)
+)
 const App = () => {
   return <RouterProvider router={router} />
 }
--- react-course/src/components/Navbar.jsx
+++ react-course/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from "../assets/images/logo.png"
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -10,7 +11,7 @@ const Navbar = () => {
             className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
           >
             {/* <!-- Logo --> */}
-            <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
+            <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
               <img
                 className="h-10 w-auto"
                 src={ logo }
@@ -19,24 +20,21 @@ const Navbar = () => {
               <span className="hidden md:block text-white text-2xl font-bold ml-2"
                 >React Jobs</span
               >
-            </a>
+            </NavLink>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
-                <a
-                  href="/index.html"
+                <NavLink
+                  to="/"
                   className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Home</a
-                >
-                <a
-                  href="/jobs.html"
+                  >Home</NavLink>
+                <NavLink
+                  to="/jobs"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Jobs</a
-                >
-                <a
-                  href="/add-job.html"
+                  >Jobs</NavLink>
+                <NavLink
+                  to="/add-job"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Add Job</a
-                >
+                  >Add Job</NavLink>
               </div>
             </div>
           </div>
