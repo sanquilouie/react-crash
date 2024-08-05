5bb56750c5145c811ee8e86f2c39e3305a8ecdd6
louieeeee <louiesanqui0518@gmail.com>
2024-08-05 18:24:05 +0800

    Added Add Jobs

--- react-course/src/App.jsx
+++ react-course/src/App.jsx
@@ -4,20 +4,27 @@ import HomePage from './pages/HomePage';
 import MainLayout from './layout/MainLayout';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
-import JobPage from './pages/JobPage';
+import JobPage, { jobLoader } from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path='/' element={<MainLayout />}>
-    <Route index element={<HomePage />} />
-    <Route path='/jobs' element={<JobsPage />} />
-    <Route path='/jobs/:id' element={<JobPage />} />
-    <Route path='*' element={<NotFoundPage />} />
 
-  </Route>
-)
-)
 const App = () => {
+  const addJob = (newJob) => {
+    console.log(newJob)
+  }
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+      <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+      <Route path='*' element={<NotFoundPage />} />
+  
+    </Route>
+  )
+  )
   return <RouterProvider router={router} />
 }
 
