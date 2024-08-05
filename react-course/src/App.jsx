425d3c733683ec83a1e3a6f14935af75a36abd7f
louieeeee <louiesanqui0518@gmail.com>
2024-08-05 15:25:25 +0800

    Added Individual Job Page

--- react-course/src/App.jsx
+++ react-course/src/App.jsx
@@ -1,15 +1,17 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import React from 'react';
 import HomePage from './pages/HomePage';
 import MainLayout from './layout/MainLayout';
 import JobsPage from './pages/JobsPage';
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import NotFoundPage from './pages/NotFoundPage';
+import JobPage from './pages/JobPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route index element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
+    <Route path='/jobs/:id' element={<JobPage />} />
     <Route path='*' element={<NotFoundPage />} />
 
   </Route>
